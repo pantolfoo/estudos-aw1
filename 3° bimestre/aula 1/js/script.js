cf5bb8b3035d15d5b09eec174f6fadedb0e89019
isa <isabella.pantolfo1101@gmail.com>
2024-08-13 11:37:04 -0300

    deu nada certo

---
+++ 3° bimestre/aula 1/js/script.js
@@ -0,0 +1,23 @@
+console.log("isinha"); // aparece no console do site (inspecionar)
+
+var nome = "isabella"; // declaracao de variavel (opcionalmente, inicializando-a com um valor) || global e local
+let idade = 17; // declaracao de variavel (opcionalmente, inicializando-a com um valor) || local - escopo de boloco
+const PI = 3.14; // declara variavel constante]
+var numero = 2;
+
+
+// alerta:
+// window.alert ('criou??')
+
+// funcao:
+var x = prompt ("digite um valor");
+
+function fatorial(x){
+    if (x === 0 || x === 1){
+        return 1;
+    }
+
+    else{
+        return 0;
+    }
+}
